5676e2a318aa257040606cb81c5b36d6b0e1128d
veetvqn <vietqnagooyan@gmail.com>
2024-03-01 17:27:48 -0600

    Created the createPizza method

--- Homework_5/src/edu/mu/pizza/PizzaCookingFactory.java
+++ Homework_5/src/edu/mu/pizza/PizzaCookingFactory.java
@@ -2,4 +2,29 @@ package edu.mu.pizza;
 
 public class PizzaCookingFactory {
 	
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		
+		switch(pizzaType.getClass().getSimpleName()) {
+		
+		case "HawaiianPizza":
+			HawaiianPizza newHawaiian = new HawaiianPizza();
+			newHawaiian.setPizzaOrderID(HawaiianPizza.getOrderIDCounter());
+			
+		case "MargheritaPizza":
+			MargheritaPizza newMargherita = new MargheritaPizza();
+			newMargherita.setPizzaOrderID(MargheritaPizza.getOrderIDCounter());
+			
+		case "SupremePizza":
+			SupremePizza newSupreme = new SupremePizza();
+			newSupreme.setPizzaOrderID(SupremePizza.getOrderIDCounter());
+			
+		case "VegetarianPizza":
+			VegatarianPizza newVegatarian = new VegatarianPizza();
+			newVegatarian.setPizzaOrderID(VegatarianPizza.getOrderIDCounter());
+			
+		default:
+			break;
+		}
+		return null;
+	}
 }

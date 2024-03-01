309697ed7d10c0195443a24e91b584ecf69b63cc
zachrsnyder <zachrsnyder2004@gmail.com>
2024-03-01 16:06:26 -0600

    Added the PizzaOrder constructor, also added the printListOfToppingsByOrderID method. I will say I am proud of how I wrote that one!

--- Homework_5/src/edu/mu/pizza/PizzaOrder.java
+++ Homework_5/src/edu/mu/pizza/PizzaOrder.java
@@ -1,5 +1,6 @@
 package edu.mu.pizza;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.mu.cookingStrategy.ICookingStrategy;
@@ -9,6 +10,30 @@ public class PizzaOrder {
 	private PizzaCookingFactory pizzaFactory;
 	private ICookingStrategy cookingStrategy;
 	private List<AbstractPizza> pizzaOrderList;
+	
+	/*
+	 * PizzaOrder:
+	 * Constructor
+	 */
+	public PizzaOrder(){
+		this.pizzaFactory = new PizzaCookingFactory();
+		this.pizzaOrderList = new ArrayList<AbstractPizza>();
+	}
+	
+	/*
+	 * printListOfToppingsByPizzaOrderID
+	 * @param orderID
+	 * uses for each loop to go through every pizza in the order list.
+	 * Once the correct pizza order is found, 
+	 */
+	public void printListOfToppingsByPizzaOrderID(int orderID) {
+		for(AbstractPizza pizza : pizzaOrderList) {
+			if(pizza.getPizzaOrderID() == orderID) {
+				System.out.println("Toppings:");
+				pizza.toppingsList.stream().map(i -> "\t" + i + "\n").forEach(System.out::print);
+			}
+		}
+	}
 	
 	
 	

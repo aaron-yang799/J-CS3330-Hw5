33be9a85c73955a0eead9648bce629523230b1e1
aaron-yang799 <aaron.yang799@gmail.com>
2024-03-11 17:23:10 -0500

    Finished testing code

--- Homework_5/src/edu/mu/Main.java
+++ Homework_5/src/edu/mu/Main.java
@@ -7,7 +7,7 @@ import edu.mu.pizza.Toppings;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// Instantiate a pizzaOrder, perform operations based on the requirements.
         PizzaOrder order = new PizzaOrder();
         // Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart,
@@ -17,12 +17,23 @@ public class Main {
         order.addPizzaToCart(PizzaType.HAWAIIAN);
         order.selectCookingStrategyByPizzaOrderID(1, CookingStyleType.MICROWAVE);
         order.addNewToppingToPizza(1, Toppings.PEPPERONI);
+        System.out.println("\nPizza after adding Pepperoni: ");
         order.printListOfToppingsByPizzaOrderID(1);
-        order.removeToppingFromPizza(1, Toppings.PEPPERONI);
-        order.addPizzaToCart(PizzaType.SUPREME);
-        System.out.println(order.isThereAnyUncookedPizza());
+        System.out.println("\nResult of removing Pepperoni: " + order.removeToppingFromPizza(1, Toppings.PEPPERONI));
+        System.out.println("\nPizza after removing Pepperoni: ");
         order.printListOfToppingsByPizzaOrderID(1);
+        System.out.println("\nResult of adding a uncooked Supreme pizza: " + order.addPizzaToCart(PizzaType.SUPREME));
+        System.out.println("\nResult of isThereAnyUncookedPizza: " + order.isThereAnyUncookedPizza() + "\n");
+        
+        System.out.println("\nAdding muliple pizzas and cooking");
+        order.addPizzaToCart(PizzaType.MARGHERITA);
+        order.addPizzaToCart(PizzaType.VEGETARIAN);
+        order.selectCookingStrategyByPizzaOrderID(2, CookingStyleType.BRICK_OVEN);
+        order.selectCookingStrategyByPizzaOrderID(3, CookingStyleType.CONVENTIONAL_OVEN);
+        order.selectCookingStrategyByPizzaOrderID(4, CookingStyleType.MICROWAVE);
+        System.out.println("\nList of pizzas in cart: ");
         order.printPizzaOrderCart(1);
+        System.out.println("Checkout total: " + order.checkout());
 	}
 
 }
